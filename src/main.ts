f3edc7310989855430d9307ede2a622210973340
spike <guang.spike.yang@gmail.com>
2025-04-10 21:20:01 +1000

    format HTML and TypeScript code for indent consistency

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import './style.css'
 const jsPsych = initJsPsych();
 
 const hello_trial = {
-  type: htmlKeyboardResponse,
-  stimulus: 'Hello world!',
-  choices: ["NO_KEYS"],
+    type: htmlKeyboardResponse,
+    stimulus: 'Hello world!',
+    choices: ["NO_KEYS"],
 } satisfies Partial<TrialType<typeof htmlKeyboardResponse.info>>
 
 jsPsych.run([hello_trial]);
